fe3e7e23c2b163b5b02b239cf2bc5663d75ea497
Lucas Duarte <lucaseddev@gmail.com>
2022-03-10 19:11:59 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Styles from './spinner-styles.scss';
+
+type Props = React.HTMLAttributes<HTMLElement>;
+
+export const Spinner: React.FC<Props> = (props: Props) => {
+  const { className } = props;
+  return (
+    <div {...props} className={[Styles.spinner, className].join(' ')}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};

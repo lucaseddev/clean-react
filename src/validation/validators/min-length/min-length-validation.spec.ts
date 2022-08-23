e408993775ce3eb7f61db7b6acb41e67fc68c4aa
Lucas Duarte <lucaseddev@gmail.com>
2022-08-23 17:27:14 -0300

    test: adding min length validation for when value has length valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,12 +1,21 @@
-import { InvalidFieldError } from "@/validation/errors";
-import { MinLengthValidation } from "./min-length-validation";
+import { InvalidFieldError } from '@/validation/errors';
+import faker from '@faker-js/faker';
+import { MinLengthValidation } from './min-length-validation';
 
 describe('Min Length Validation', () => {
   it('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 5);
+    const sut = new MinLengthValidation(faker.database.column(), 5);
 
     const error = sut.validate('123');
 
-    expect(error).toEqual(new InvalidFieldError(sut.field))
+    expect(error).toEqual(new InvalidFieldError(sut.field));
+  });
+
+  it('Should return null if value has valid length', () => {
+    const sut = new MinLengthValidation(faker.database.column(), 5);
+
+    const error = sut.validate('123456');
+
+    expect(error).toBeFalsy();
   });
 });

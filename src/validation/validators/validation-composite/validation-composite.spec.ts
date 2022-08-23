411b181516c1069f2cf9549a9e627c2f2bd3bc0d
Lucas Duarte <lucaseddev@gmail.com>
2022-08-23 18:13:18 -0300

    refactor: move sut creation to factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -2,20 +2,34 @@ import faker from '@faker-js/faker';
 import { ValidationSpy } from '../test/mock-field-validation';
 import { ValidationComposite } from './validation-composite';
 
+type TSutTypes = {
+  sut: ValidationComposite;
+  fieldValidationsSpy: ValidationSpy[];
+};
+
+const makeSut = (field: string): TSutTypes => {
+  const fieldValidationsSpy = [
+    new ValidationSpy(field),
+    new ValidationSpy(field),
+  ];
+
+  const sut = new ValidationComposite(fieldValidationsSpy);
+
+  return {
+    sut,
+    fieldValidationsSpy,
+  };
+};
+
 describe('ValidationComposite', () => {
   it('Should return error if any validation fails', () => {
     const field = faker.database.column();
-    const fieldValidationSpySuccess = new ValidationSpy(field);
-    const fieldValidationSpy2Error = new ValidationSpy(field);
 
-    const errorMsg = 'Any_Message';
-    fieldValidationSpy2Error.error = new Error(errorMsg);
+    const { sut, fieldValidationsSpy } = makeSut(field);
 
-    const sut = new ValidationComposite([
-      fieldValidationSpySuccess,
-      fieldValidationSpy2Error,
-    ]);
-
+    const errorMsg = faker.random.word();
+    fieldValidationsSpy[1].error = new Error(errorMsg);
+    
     const error = sut.validate(field, faker.random.word());
 
     expect(error).toBe(errorMsg);
@@ -23,18 +37,13 @@ describe('ValidationComposite', () => {
 
   it('Should return first validation error that fails', () => {
     const field = faker.database.column();
-    const fieldValidationSpy1Error = new ValidationSpy(field);
-    const fieldValidationSpy2Error = new ValidationSpy(field);
+
+    const { sut, fieldValidationsSpy } = makeSut(field);
 
     const errorMsg1 = faker.random.word();
     const errorMsg2 = faker.random.word();
-    fieldValidationSpy1Error.error = new Error(errorMsg1);
-    fieldValidationSpy2Error.error = new Error(errorMsg2);
-
-    const sut = new ValidationComposite([
-        fieldValidationSpy1Error,
-      fieldValidationSpy2Error,
-    ]);
+    fieldValidationsSpy[0].error = new Error(errorMsg1);
+    fieldValidationsSpy[1].error = new Error(errorMsg2);
 
     const error = sut.validate(field, faker.random.word());
 

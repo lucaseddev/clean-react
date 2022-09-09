d6018d329dd1024902b0cbd44ed1e2044a06312d
Lucas Duarte <lucaseddev@gmail.com>
2022-09-09 13:35:28 -0300

    chore: ignoring index files on coverage

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts, tsx}',
     '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/domain/**/index.ts',
-    '!<rootDir>/src/validation/**/index.ts',
+    '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts',
   ],
   coverageDirectory: 'coverage',

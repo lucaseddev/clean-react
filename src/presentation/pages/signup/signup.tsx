04b9ea8329cfd3e0ec744e30658577a822ae46e5
Lucas Duarte <lucaseddev@gmail.com>
2022-09-12 12:11:16 -0300

    feat: ensure SignUp starts with all fields required

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,36 +1,69 @@
-import { Button, Footer, FormStatus, Input, LoginHeader } from '@/presentation/components';
+import {
+  Button,
+  Footer,
+  FormStatus,
+  Input,
+  LoginHeader,
+} from '@/presentation/components';
 import { FormContext } from '@/presentation/contexts';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Styles from './signup.styles.scss';
 
 export const SignUp: React.FC = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    errorMessage: '',
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+  });
+
   return (
     <div className={Styles.signup}>
       <LoginHeader />
 
-      <FormContext.Provider value={{ state: {} }}>
+      <FormContext.Provider value={{ state }}>
         <form className={Styles.form}>
           <h2>Login</h2>
-          <Input name="name" type="text" placeholder="Digite seu nome" />
-          <Input name="email" type="email" placeholder="Digite seu e-mail" />
+          <Input
+            name="name"
+            type="text"
+            placeholder="Digite seu nome"
+            errorMessage={state.nameError}
+          />
+          <Input
+            name="email"
+            type="email"
+            placeholder="Digite seu e-mail"
+            errorMessage={state.emailError}
+          />
           <Input
             name="password"
             type="password"
             placeholder="Digite sua senha"
+            errorMessage={state.passwordError}
           />
           <Input
             name="passwordConfirmation"
             type="password"
             placeholder="Repita sua senha"
+            errorMessage={state.passwordConfirmationError}
           />
 
-          <Button className={Styles.submit} type="submit">
+          <Button
+            data-testid="submit"
+            disabled
+            className={Styles.submit}
+            type="submit">
             Entrar
           </Button>
-          <Link to="/login" className={Styles.link}>
+          <span
+            // to="/login"
+            className={Styles.link}>
             Voltar para Login
-          </Link>
+          </span>
 
           <FormStatus />
         </form>
